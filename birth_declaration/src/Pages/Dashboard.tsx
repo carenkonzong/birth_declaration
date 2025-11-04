e6b1929782290df8c5dd63591042cd75faa4b0df
Edwin Tsafack Nkonzong <tsafackedwin@gmail.com>
2025-11-04 00:33:59 -0500

    Add NoDeclaration component and update Dashboard to handle empty declarations state

--- birth_declaration/src/Pages/Dashboard.tsx
+++ birth_declaration/src/Pages/Dashboard.tsx
@@ -2,17 +2,27 @@ import All_declarations from "../components/All_declarations";
 import Card_parent from "../components/Card_parent";
 import Top_bar from "../components/Top_bar";
 import useDeclarations from "../hooks/use-declarations/useDeclarations";
+import NoDeclaration from "../components/noDeclaration";
 
 function Dashboard() {
   const { declarations } = useDeclarations();
 
-  return (
-    <main>
-      <Top_bar />
-      <Card_parent declarations={declarations} />
-      <All_declarations declarations={declarations} />
-    </main>
-  );
+  if (declarations.length === 0) {
+    return (
+      <>
+        <Top_bar />
+        <NoDeclaration />
+      </>
+    );
+  } else {
+    return (
+      <main>
+        <Top_bar />
+        <Card_parent declarations={declarations} />
+        <All_declarations declarations={declarations} />
+      </main>
+    );
+  }
 }
 
 export default Dashboard;
--- birth_declaration/src/hooks/index.ts
+++ birth_declaration/src/hooks/index.ts
@@ -1,2 +1,1 @@
-/* export * from "./use-declarations/useDeclarations";
- */
+export * from "./use-declarations/useDeclarations";
