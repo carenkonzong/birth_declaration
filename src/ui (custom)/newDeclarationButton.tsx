94f16d2111157a3196e795444fd4fdbfceec654d
Edwin Tsafack Nkonzong <tsafackedwin@gmail.com>
2026-01-22 15:42:15 -0500

    Add NewDeclarationButton component for creating new declarations with a styled link

---
+++ src/ui (custom)/newDeclarationButton.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { CirclePlus } from "lucide-react";
+
+function NewDeclarationButton() {
+  return (
+    <div className="flex items-center">
+      <Link
+        to="/new_declaration"
+        className="cursor-pointer px-6 py-3 rounded-xl text-white flex bg-linear-to-r from-[#3582f8] via-[#a25de3] to-[#23bf72] "
+      >
+        <div className="mr-3 flex items-center">
+          <CirclePlus size={20} />
+        </div>
+        New Declaration
+      </Link>
+    </div>
+  );
+}
+
+export default NewDeclarationButton;
